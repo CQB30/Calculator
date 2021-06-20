b7d471f9e6675714d26dcd8a4caeece9892126dd
cqb30 <cqb30al@gmail.com>
2021-06-19 20:34:22 -0400

    Finished Calculator and Added UI

--- calculator.js
+++ calculator.js
@@ -5,45 +5,91 @@ const equalsButton = document.querySelector("#equals");
 const clearButton = document.querySelector('#clear');
 
 function add(num1, num2) {
-  return num1 + num2
+  return parseFloat(num1) + parseFloat(num2)
 };
 
 function subtract(num1, num2) {
-	return num1 - num2
+	return parseFloat(num1) - parseFloat(num2)
 };
 
 function multiply(num1, num2) {
-  return num1 * num2
+  return parseFloat(num1) * parseFloat(num2)
 }
 
 function divide(num1, num2) {
-  return num1 / num2
+  return parseFloat(num1) / parseFloat(num2)
 }
 
 function operate(operator, num1, num2) {
-  switch (operator) {
-    case "+":
-      return add(num1, num2)
-      break;
-    case "-":
-      return subtract(num1, num2)
-      break;
-    case "*":
-      return multiply(num1, num2)
-      break;
-    case "/":
-      return divide(num1, num2)
-      break;
+  if (operator == "+") {
+      return Math.round(add(num1, num2)* 100) / 100
+  } else if (operator == "-") {
+      return Math.round(subtract(num1, num2) * 100) / 100
+  } else if (operator == "*") {
+      return Math.round(multiply(num1, num2) * 100) / 100
+  } else {
+      return Math.round(divide(num1, num2) * 100) / 100
   }
+}
+
+let operator = "";
+let number1 = "";
+let number2 = "";
+
+function readDisplay() {
+  return display.textContent
+}
+
+function resetDisplay() {
+  display.textContent = "";
 }
 
 function appendNumber(number) {
   display.textContent += number;
 }
 
+clearButton.addEventListener("click", () => {
+  resetDisplay();
+  operator = "";
+  number1 = "";
+  number2 = "";
+})
+
 digits.forEach((button) => {
   button.addEventListener("click", () => {
     appendNumber(button.textContent);
-    console.log("test");
   })
-});
+})
+
+operators.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (operator == "") {
+    operator = button.textContent; 
+    number1 = readDisplay();
+    resetDisplay();
+    } else {
+      number2 = readDisplay();
+      number1 = operate(operator, number1, number2);
+      operator = button.textContent;
+      resetDisplay();
+    }
+  })
+})
+
+equalsButton.addEventListener("click", () => {
+  if (number2 == 0 && operator == "/") {
+    alert("ERROR can't divide by 0")
+    number1 = "";
+    number2 = "";
+    operator = "";
+  } else {
+    number2 = readDisplay();
+    if (number1 == "") {
+      number1 = display.textContent;
+    } else {
+    display.textContent = operate(operator, number1, number2);
+    operator = "";
+    number1 = readDisplay();
+    }
+  }
+})
